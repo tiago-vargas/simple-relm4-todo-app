3cd0139c810d68af24e65747acd50510f933d569
Tiago Vargas <tiago.ce10@outlook.com>
2023-11-22 15:52:44 -0300

    Move window state things to `settings`
    I think it makes more sense to put them in `settings` as they make
    intense use of the `Settings` enum.
    
    It also cleans `app` a bit.

--- src/app/settings.rs
+++ src/app/settings.rs
@@ -1,3 +1,8 @@
+use gtk::prelude::*;
+use relm4::prelude::*;
+
+use super::{AppModel, APP_ID};
+
 pub(crate) enum Settings {
     WindowWidth,
     WindowHeight,
@@ -10,6 +15,35 @@ impl Settings {
             Self::WindowWidth => "window-width",
             Self::WindowHeight => "window-height",
             Self::WindowIsMaximized => "window-is-maximized",
+        }
+    }
+}
+
+impl AppModel {
+    pub(crate) fn save_window_state(widgets: &<Self as SimpleComponent>::Widgets) {
+        let settings = gtk::gio::Settings::new(APP_ID);
+
+        let (width, height) = widgets.main_window.default_size();
+        let _ = settings.set_int(Settings::WindowWidth.as_str(), width);
+        let _ = settings.set_int(Settings::WindowHeight.as_str(), height);
+
+        let _ = settings.set_boolean(
+            Settings::WindowIsMaximized.as_str(),
+            widgets.main_window.is_maximized(),
+        );
+    }
+
+    pub(crate) fn load_window_state(widgets: &<Self as SimpleComponent>::Widgets) {
+        let settings = gtk::gio::Settings::new(APP_ID);
+
+        let width = settings.int(Settings::WindowWidth.as_str());
+        let height = settings.int(Settings::WindowHeight.as_str());
+
+        widgets.main_window.set_default_size(width, height);
+
+        let is_maximized = settings.boolean(Settings::WindowIsMaximized.as_str());
+        if is_maximized {
+            widgets.main_window.maximize();
         }
     }
 }
